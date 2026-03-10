564a48f8273eaa5c1b488796fffe33fe0c524bf4
Marco Monteiro <kanxuk@gmail.com>
2026-03-10 17:09:22 +0000

    Added Logs COmmand

--- test.php
+++ test.php
@@ -2,9 +2,9 @@
 require 'vendor/autoload.php';
 
 use ApiCrumbs\Core\ApiCrumbs;
-use ApiCrumbs\Providers\Geo\PostcodeProvider;
+use ApiCrumbs\Providers\Geo\PostcodeIoProvider;
 
 $engine = new ApiCrumbs();
-$engine->registerProvider(new PostcodeProvider());
+$engine->registerProvider(new PostcodeIoProvider($guzzleConfig = ['verify' => false]));
 
 echo $engine->build('SW1A     1AA');
